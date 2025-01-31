a53a2f6384a65f2e348347537eff7b352379da58
mrbonk97 <mrbonk97@gmail.com>
2025-02-01 00:13:04 +0900

    로컬 폰트 적용
    프리텐다드 & 마루부리 사용

---
+++ lib/font.ts
@@ -0,0 +1,33 @@
+import localFont from "next/font/local";
+
+export const pretendard = localFont({ src: "./fonts/PretendardVariable.woff2" });
+
+export const maruburi = localFont({
+  src: [
+    {
+      path: "./fonts/maruburi/MaruBuri-Bold.woff2",
+      weight: "400",
+      style: "bold",
+    },
+    {
+      path: "./fonts/maruburi/MaruBuri-SemiBold.woff2",
+      weight: "600",
+      style: "semi-bold",
+    },
+    {
+      path: "./fonts/maruburi/MaruBuri-Regular.woff2",
+      weight: "400",
+      style: "regular",
+    },
+    {
+      path: "./fonts/maruburi/MaruBuri-Light.woff2",
+      weight: "300",
+      style: "light",
+    },
+    {
+      path: "./fonts/maruburi/MaruBuri-ExtraLight.woff2",
+      weight: "200",
+      style: "extra-light",
+    },
+  ],
+});
