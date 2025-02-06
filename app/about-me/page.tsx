cb3bbe15b5577557eb5a1a7cc22b9eebbc124425
mrbonk97 <mrbonk97@gmail.com>
2025-02-07 01:00:07 +0900

    /about-me 페이지 구현

---
+++ app/about-me/page.tsx
@@ -0,0 +1,89 @@
+import { ProjectSection } from "@/components/card/project-section";
+import { RESUME } from "@/constants/resume";
+
+const AboutMePage = () => {
+  return (
+    <main className="pt-[6.25rem] pb-20 px-5 mx-auto max-w-screen-xl space-y-36 md:space-y-52">
+      <hgroup className="pt-20">
+        <h1 className="text-5xl font-bold text-center leading-snug break-keep opacity-80">
+          안녕하세요 개발자 <strong>김현석</strong>입니다.
+        </h1>
+        <p className="mt-10 mx-auto max-w-screen-md text-lg font-medium text-center opacity-80">
+          저는 개발자를 목표로 삼고 매일 배우며 성장하고 있습니다. 프로그래밍은 물론 프로젝트 관리,
+          웹 취약점 점검 등 다양한 분야에서 경험을 쌓아왔으며, 이러한 폭넓은 경험이 저의 가장 큰
+          강점이라 생각합니다.
+        </p>
+      </hgroup>
+      <ProjectSection header="경력">
+        <ul className="mt-10 md:mt-20 mx-auto max-w-screen-sm space-y-10">
+          {RESUME.career.map((item) => (
+            <li key={item.company} className="font-bold opacity-80 flex justify-between gap-5">
+              <div className="text-2xl md:text-5xl">{item.company}</div>
+              <div className="text-right">
+                <div className="md:text-2xl">{item.duration}</div>
+                <div className="text-sm md:text-base">
+                  {item.department}/{item.position}
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </ProjectSection>
+      <ProjectSection header="학력">
+        <ul className="mt-10 md:mt-20 mx-auto max-w-screen-sm space-y-10">
+          {RESUME.school.map((item) => (
+            <li key={item.school} className="font-bold opacity-80 flex justify-between gap-5">
+              <div className="text-2xl md:text-5xl">{item.school}</div>
+              <div className="text-right">
+                <div className="md:text-2xl">{item.duration}</div>
+                <div className="text-sm md:text-base">{item.major}</div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </ProjectSection>
+      <ProjectSection header="교육">
+        <ul className="mt-10 md:mt-20 mx-auto max-w-screen-sm space-y-10">
+          {RESUME.education.map((item) => (
+            <li key={item.name} className="font-bold opacity-80 flex justify-between gap-5">
+              <div className="text-2xl md:text-5xl">{item.name}</div>
+              <div className="text-right">
+                <div className="md:text-2xl">{item.duration}</div>
+                <div className="text-sm md:text-base">{item.course}</div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </ProjectSection>
+      <ProjectSection header="자격증">
+        <ul className="mt-10 md:mt-20 mx-auto max-w-screen-sm space-y-10">
+          {RESUME.certificate.map((item) => (
+            <li key={item.name} className="font-bold opacity-80 flex justify-between gap-5">
+              <div className="text-2xl md:text-5xl">{item.name}</div>
+              <div className="text-right">
+                <div className="md:text-2xl">{item.duration}</div>
+                <div className="text-sm md:text-base">{item.issuer}</div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </ProjectSection>
+      <ProjectSection header="스킬">
+        <ul className="mt-10 md:mt-20 mx-auto max-w-screen-sm space-y-10">
+          {RESUME.skills.map((item) => (
+            <li key={item.type} className="font-bold opacity-80 flex justify-between gap-5">
+              <div className="text-2xl md:text-5xl">{item.type}</div>
+              <div className="text-right text-sm md:text-base space-x-5">
+                {item.list.map((item2) => (
+                  <span key={item2}>{item2}</span>
+                ))}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </ProjectSection>
+    </main>
+  );
+};
+
+export default AboutMePage;
---
+++ constants/resume.ts
@@ -0,0 +1,59 @@
+export const RESUME = {
+  id: "career",
+  name: "경력",
+  career: [
+    {
+      company: "하나금융TI",
+      department: "정보보호센터",
+      position: "사원",
+      duration: "2024.11 - 재직중",
+    },
+    {
+      company: "한전KDN",
+      department: "원전ERP고도화TF",
+      position: "사원",
+      duration: "2022.11 - 2024.02",
+    },
+  ],
+  school: [
+    {
+      school: "전남대학교",
+      major: "컴퓨터정보통신공학과",
+      duration: "2016.03 - 2023.02",
+    },
+  ],
+  education: [
+    {
+      name: "한국폴리텍",
+      course: "하이테크과정",
+      duration: "2024.03 - 2024.11",
+    },
+  ],
+  certificate: [
+    {
+      name: "정보처리기사",
+      issuer: "한국산업인력공단",
+      duration: "2022.06",
+    },
+    {
+      name: "SQLD",
+      issuer: "한국데이터산업진흥원",
+      duration: "2024.05",
+    },
+    {
+      name: "TOEIC",
+      issuer: "한국YBM",
+      duration: "2024.04",
+    },
+  ],
+  skills: [
+    {
+      type: "언어",
+      list: ["Typescript", "Java", "SQL"],
+    },
+    {
+      type: "프레임워크",
+      list: ["Nextjs", "스프링부트"],
+    },
+  ],
+};
